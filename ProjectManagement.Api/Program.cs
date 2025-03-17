bfa6f37ffafcc0b64d3cbb3ac24dda6af7335ad7
Az1zbekErgashev <ergasevaziz0611@gmail.com>
2025-03-17 23:32:23 +0900

    fixed some bugs

--- ProjectManagement.Api/Program.cs
+++ ProjectManagement.Api/Program.cs
@@ -1,34 +1,42 @@
 using Microsoft.AspNetCore.HttpOverrides;
-using Microsoft.AspNetCore.Mvc.ApplicationModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
 using ProjectManagement.Api.Extensions;
 using ProjectManagement.Infrastructure.Contexts;
 using ProjectManagement.Service.Extencions;
 using ProjectManagement.Service.Interfaces.IRepositories;
 using ProjectManagement.Service.Service.Repositories;
-using System.Text;
-using System.Text.Json.Serialization;
-using static ProjectManagement.Service.Service.Attachment.AttachmentService;
 using Serilog;
-using Microsoft.Extensions.Logging;
+using static ProjectManagement.Service.Service.Attachment.AttachmentService;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// ¬°¬á¬â¬Ö¬Õ¬Ö¬Ý¬ñ¬Ö¬Þ ¬ä¬Ö¬Ü¬å¬ë¬å¬ð ¬ã¬â¬Ö¬Õ¬å
+var environment = builder.Environment.EnvironmentName;
+
 builder.Services.AddControllers();
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-var configuration = builder.Configuration;
 
+builder.Configuration.SetBasePath(Directory.GetCurrentDirectory());
+builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                     .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                     .AddEnvironmentVariables();
+
+
+// ¬¯¬Ñ¬ã¬ä¬â¬à¬Û¬Ü¬Ñ ForwardedHeadersOptions ¬Õ¬Ý¬ñ ¬â¬Ñ¬Ò¬à¬ä¬í ¬ã ¬á¬â¬à¬Ü¬ã¬Ú-¬ã¬Ö¬â¬Ó¬Ö¬â¬Ñ¬Þ¬Ú
 builder.Services.Configure<ForwardedHeadersOptions>(options =>
 {
     options.ForwardedHeaders =
         ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
 });
+
+// ¬°¬ä¬Ü¬Ý¬ð¬é¬Ñ¬Ö¬Þ ¬Ñ¬Ó¬ä¬à¬Þ¬Ñ¬ä¬Ú¬é¬Ö¬ã¬Ü¬à¬Ö ¬Ó¬Ñ¬Ý¬Ú¬Õ¬Ñ¬è¬Ú¬ð ¬Þ¬à¬Õ¬Ö¬Ý¬Ú
 builder.Services.Configure<ApiBehaviorOptions>(options =>
 {
     options.SuppressModelStateInvalidFilter = true;
 });
 
+// ¬²¬Ñ¬Ù¬â¬Ö¬ê¬Ñ¬Ö¬Þ CORS ¬Õ¬Ý¬ñ ¬Ó¬ã¬Ö¬ç ¬Ù¬Ñ¬á¬â¬à¬ã¬à¬Ó
 builder.Services.AddCors(e => e.AddDefaultPolicy(e =>
             e.AllowAnyHeader()
                 .AllowAnyOrigin()
@@ -37,29 +45,30 @@ builder.Services.AddCors(e => e.AddDefaultPolicy(e =>
     )
 );
 
+// ¬¬¬à¬ß¬æ¬Ú¬Ô¬å¬â¬Ñ¬è¬Ú¬ñ Swagger
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.DocumentFilter<LowercaseDocumentFilter>();
+});
 
-builder.Services.AddSwaggerGen();
-
-
+// ¬¯¬Ñ¬ã¬ä¬â¬à¬Û¬Ü¬Ñ ¬á¬à¬Õ¬Ü¬Ý¬ð¬é¬Ö¬ß¬Ú¬ñ ¬Ü ¬Ò¬Ñ¬Ù¬Ö ¬Õ¬Ñ¬ß¬ß¬í¬ç PostgreSQL
 var connectionString = builder.Configuration.GetConnectionString("PostgresConnection");
 builder.Services.AddDbContext<ProjectManagementDB>(options =>
 {
     options.UseNpgsql(connectionString, b => b.MigrationsAssembly("ProjectManagement.Api"));
     options.EnableDetailedErrors();
 });
+
+// ¬£¬Ü¬Ý¬ð¬é¬Ñ¬Ö¬Þ ¬á¬à¬Õ¬Õ¬Ö¬â¬Ø¬Ü¬å LegacyTimestampBehavior ¬Õ¬Ý¬ñ Npgsql
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+// ¬¥¬à¬Ò¬Ñ¬Ó¬Ý¬ñ¬Ö¬Þ ¬á¬à¬Õ¬Õ¬Ö¬â¬Ø¬Ü¬å ¬Ñ¬Ó¬ä¬à¬â¬Ú¬Ù¬Ñ¬è¬Ú¬Ú
 builder.Services.AddAuthorization();
 
-if (string.IsNullOrEmpty(configuration["JWT:Key"]))
-{
-    var loggr = LoggerFactory.Create(builder => builder.AddConsole()).CreateLogger("Startup");
-    loggr.LogError("JWT Key is missing in the configuration.");
-    loggr.LogError(configuration["JWT"]);
-    throw new InvalidOperationException("JWT Key is missing.");
-}
 builder.Services.AddSignalR();
 
+// ¬¯¬Ñ¬ã¬ä¬â¬à¬Û¬Ü¬Ñ ¬Ý¬à¬Ô¬Ú¬â¬à¬Ó¬Ñ¬ß¬Ú¬ñ ¬ã ¬Ú¬ã¬á¬à¬Ý¬î¬Ù¬à¬Ó¬Ñ¬ß¬Ú¬Ö¬Þ Serilog
 var logger = new LoggerConfiguration()
     .ReadFrom.Configuration(builder.Configuration)
     .Enrich.FromLogContext()
@@ -69,27 +78,40 @@ var logger = new LoggerConfiguration()
 builder.Logging.ClearProviders();
 builder.Logging.AddSerilog(logger).AddConsole();
 
+// ¬±¬à¬Õ¬Ü¬Ý¬ð¬é¬Ö¬ß¬Ú¬Ö ¬Ó¬ã¬Ö¬ç ¬ã¬Ö¬â¬Ó¬Ú¬ã¬à¬Ó ¬Ú ¬Ü¬à¬ß¬æ¬Ú¬Ô¬å¬â¬Ñ¬è¬Ú¬Û
 builder.Services.ConfigureJwt(builder.Configuration);
 builder.Services.AddSwaggerService();
 builder.Services.AddCustomServices();
 builder.Services.AddHttpContextAccessor();
+
+// ¬³¬à¬Ù¬Õ¬Ñ¬Ö¬Þ ¬á¬â¬Ú¬Ý¬à¬Ø¬Ö¬ß¬Ú¬Ö
 var app = builder.Build();
 
+// ¬µ¬Ò¬Ö¬Õ¬Ú¬Þ¬ã¬ñ, ¬é¬ä¬à ¬ß¬Ñ¬ã¬ä¬â¬à¬Û¬Ü¬Ú Swagger ¬ä¬à¬Ý¬î¬Ü¬à ¬Õ¬Ý¬ñ Development
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-app.Services.GetRequiredService<IWebHostEnvironment>();
+
+// ¬±¬à¬Ý¬å¬é¬Ñ¬Ö¬Þ ¬ä¬Ö¬Ü¬å¬ë¬å¬ð ¬ã¬â¬Ö¬Õ¬å
+var env = app.Environment.EnvironmentName;
+
+// ¬£¬Ü¬Ý¬ð¬é¬Ñ¬Ö¬Þ CORS
 app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
+// ¬¯¬Ñ¬ã¬ä¬â¬à¬Û¬Ü¬Ñ WebRootPath
 EnvironmentHelper.WebRootPath = app.Services.GetRequiredService<IWebHostEnvironment>()?.WebRootPath;
+
+// ¬³¬ä¬Ñ¬ä¬Ú¬é¬Ö¬ã¬Ü¬Ú¬Ö ¬æ¬Ñ¬Û¬Ý¬í
 app.UseStaticFiles();
+
+// ¬¡¬å¬ä¬Ö¬ß¬ä¬Ú¬æ¬Ú¬Ü¬Ñ¬è¬Ú¬ñ ¬Ú ¬â¬Ö¬Õ¬Ú¬â¬Ö¬Ü¬ä ¬ß¬Ñ HTTPS
 app.UseAuthentication();
 app.UseHttpsRedirection();
-//app.UseMiddleware<ProjectManagementExceptionMiddlewares>();
 app.UseAuthorization();
 
+// ¬®¬Ñ¬â¬ê¬â¬å¬ä¬í
 app.MapControllers();
 
 app.Run();
